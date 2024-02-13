2666ec432c5c07dd5e54c1492ef8c4f1d61cba89
Ali Dorostkar <ashkan.dorostkar@gmail.com>
2024-02-13 08:34:40 +0000

    Correction to build step for fzf

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,7 +2,7 @@ local M = {
     {
         'junegunn/fzf.vim',
         dependencies = {
-            { 'junegunn/fzf', build = 'install', },
+            { 'junegunn/fzf', build = './install', },
         },
         keys = {
             { "<Right>", "<cmd> Files<CR>", "Open fuzzy file finder",},
