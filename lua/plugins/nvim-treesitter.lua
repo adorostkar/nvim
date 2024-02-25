723a1deb7cb8b9c750714a4901bbd74c2b05c827
Ali Dorostkar <ashkan.dorostkar@gmail.com>
2024-02-25 20:02:46 +0000

    Markdown added to treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -6,7 +6,7 @@ local M = {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc" },
+                ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
