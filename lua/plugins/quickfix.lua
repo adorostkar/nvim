25d7297dee14f9cce78798b583984482e7a7bbeb
Ali Dorostkar <ashkan.dorostkar@gmail.com>
2024-03-01 23:12:31 +0000

    Removed bqf

--- lua/plugins/quickfix.lua
+++ lua/plugins/quickfix.lua
@@ -3,13 +3,13 @@ local M = {
         'sk1418/QFGrep',
         ft = 'qf',
     },
-    { -- quickfix preview
-        'kevinhwang91/nvim-bqf',
-        ft = 'qf',
-        dependencies = {
-            'junegunn/fzf',
-        },
-    },
+    -- { -- quickfix preview, too much noise and very slow on large quickfix
+    --     'kevinhwang91/nvim-bqf',
+    --     ft = 'qf',
+    --     dependencies = {
+    --         'junegunn/fzf',
+    --     },
+    -- },
 }
 
 return M
