0daa4a8f6008bd4882c3dda2f979c1ec0a6d6672
Ali Dorostkar <ashkan.dorostkar@gmail.com>
2025-07-14 09:43:47 +0000

    Update mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 local M = {
     {
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
         opts = {
             ensure_installed = {
                 "clangd",
@@ -26,30 +26,24 @@ local M = {
     },
     { 'neovim/nvim-lspconfig' },
     {
-        "williamboman/mason-lspconfig.nvim",
+        "mason-org/mason-lspconfig.nvim",
         dependencies = {
             "williamboman/mason.nvim",
         },
         config = function()
             require("mason").setup()
-            require("mason-lspconfig").setup()
-
-            require("mason-lspconfig").setup_handlers {
-                -- The first entry (without a key) will be the default handler
-                -- and will be called for each installed server that doesn't have
-                -- a dedicated handler.
-                function (server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {}
-                end,
-                -- Next, you can provide a dedicated handler for specific servers.
-                -- For example, a handler override for the `rust_analyzer`:
-                -- ["rust_analyzer"] = function ()
-                --     require("rust-tools").setup {}
-                -- end
+            require("mason-lspconfig").setup{
+            ensure_installed = {
+			    "clangd",
+                "lua_ls",
+                "bashls",
+                "eslint"
             }
+			}
         end,
     },
 }
 
 return M
 -- return {}
+
