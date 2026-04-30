dcbc99b1c4752349034abfa63aac6fd5ace5e329
Ali Dorostkar <ashkan.dorostkar@gmail.com>
2026-04-30 10:36:40 +0000

    Fix treesitter lazy setup

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,16 +2,27 @@ local M = {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function ()
-            local configs = require("nvim-treesitter.configs")
+        main = "nvim-treesitter.configs",
+        opts = {
+            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        },
+        config = function(_, opts)
+            local ok, configs = pcall(require, "nvim-treesitter.configs")
+            if not ok then
+                vim.schedule(function()
+                    vim.notify(
+                        "nvim-treesitter is missing or installed incorrectly. Run :Lazy sync nvim-treesitter or reinstall the plugin.",
+                        vim.log.levels.ERROR
+                    )
+                end)
+                return
+            end
 
-            configs.setup({
-                ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
-                sync_install = false,
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end
+            configs.setup(opts)
+        end,
     },
 }
 
